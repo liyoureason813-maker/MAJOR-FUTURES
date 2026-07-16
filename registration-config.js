a3ca3a231ebf6dba9892b50ba404c46939983b79
Counseling Maker <liyoureason813@gmail.com>
2026-07-16 16:45:38 +0800

    Add files via upload

---
+++ registration-config.js
@@ -0,0 +1,10 @@
+/*
+ * 家长登记配置
+ * 1. 按 SETUP_家长登记.md 部署 Google Apps Script。
+ * 2. 把部署后以 /exec 结尾的网址粘贴到 endpoint。
+ */
+window.MF_REGISTRATION_CONFIG = {
+  endpoint: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  storageKey: "major_futures_parent_access_v1",
+  siteVersion: "3.4"
+};
